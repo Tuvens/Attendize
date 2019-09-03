a668648660263e24209fb6b48ac0185904a9a78b
eret <eret@eret-Inspiron-5570>
2019-09-03 16:35:55 +0300

    important

--- resources/views/Public/ViewEvent/Partials/EventHiddenTicketsSelection.blade.php
+++ resources/views/Public/ViewEvent/Partials/EventHiddenTicketsSelection.blade.php
@@ -55,7 +55,7 @@ $is_free_event = true;
                     {!! Form::hidden('tickets[]', $ticket->id) !!}
                     <meta property="availability" content="http://schema.org/InStock">
                     <select name="ticket_{{$ticket->id}}" class="form-control"
-                            style="text-align: center;">
+                            style="text-align: center; width: auto !important;">
                         @if ($tickets->count() > 1)
                             <option value="0">0</option>
                         @endif
